ff7106726c40f875e9e76a9eaa5bd8376652e5e8
Heo <Heo@DESKTOP-7SAP0HK>
2019-07-06 23:52:05 +0900

    branch upload

---
+++ test/src/test/Merge.java
@@ -0,0 +1,19 @@
+package test;
+
+public class Merge 
+{
+	private String test;
+
+	public Merge() {
+		super();
+	}
+
+	public String getTest() {
+		return test;
+	}
+
+	public void setTest(String test) {
+		this.test = test;
+	}
+	
+}
